722fdc6d2ac8feeee14e2667af9fc954f90937ea
williamsJosh <williamsjoshua005@gmail.com>
2019-03-06 20:02:46 +0100

    initial commit

---
+++ src/main/java/com/test/javadocker/JavadockerApplication.java
@@ -0,0 +1,13 @@
+package com.test.javadocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/javadocker/JavadockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.javadocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavadockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
